fbf8c8e963df61c31f51784276c8cf184aef151a
Mohammadreza Fekri <fekri.mohammadreza86@gmail.com>
2022-07-18 23:33:34 +0430

    added adapter and data class and recyclerView

---
+++ app/src/main/java/com/app/recyclerview/ux/data/FoodData.kt
@@ -0,0 +1,13 @@
+package com.app.recyclerview.ux.data
+
+data class FoodData (
+
+    val txtSubject: String,
+    val txtPrice: String,
+    val txtDistance: String,
+    val txtCity: String,
+    val urlImage: String,
+    val numOfRating: Int,
+    val rating: Float
+
+        )
