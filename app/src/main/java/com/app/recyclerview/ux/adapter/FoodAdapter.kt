341190002ea207a96bba0e0c841ced9ac67d93b6
Mohammadreza Fekri <fekri.mohammadreza86@gmail.com>
2022-07-19 21:41:19 +0430

    added adapter and data class and recyclerView

--- app/src/main/java/com/app/recyclerview/ux/adapter/FoodAdapter.kt
+++ app/src/main/java/com/app/recyclerview/ux/adapter/FoodAdapter.kt
@@ -8,33 +8,50 @@ import android.widget.RatingBar
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.app.recyclerview.R
+import com.app.recyclerview.ux.dataclass.FoodData
 
-class FoodAdapter(): RecyclerView.Adapter<FoodAdapter.FoodViewHolder>() {
+class FoodAdapter( private val data: List<FoodData>): RecyclerView.Adapter<FoodAdapter.FoodViewHolder>() {
 
-    inner class FoodViewHolder( itemView: View ): RecyclerView.ViewHolder( itemView ) {
+
+
+    inner class FoodViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
 
         val imgMain = itemView.findViewById<ImageView>(R.id.item_img_main)
-        val txtSubject = itemView.findViewById<TextView>(R.id.item_txt_subject)
         val txtCity = itemView.findViewById<TextView>(R.id.item_txt_city)
+        val txtSubject = itemView.findViewById<TextView>(R.id.item_txt_subject)
         val txtPrice = itemView.findViewById<TextView>(R.id.item_txt_price)
         val txtDistance = itemView.findViewById<TextView>(R.id.item_txt_distance)
-        val txtRating = itemView.findViewById<TextView>(R.id.item_txt_rating)
-        val rtingBar = itemView.findViewById<RatingBar>(R.id.item_rating_main)
+        val ratingBar = itemView.findViewById<RatingBar>(R.id.item_rating_main)
+
+
+        fun bindData(position: Int) {
+
+            txtSubject.text = data[position].txtSubject
+            txtCity.text = data[position].txtCity
+            txtPrice.text = data[position].txtPrice
+            txtDistance.text = data[position].txtDistance
+            ratingBar.rating = data[position].rating
+
+        }
 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FoodViewHolder {
 
-        val view = LayoutInflater.from( parent.context ).inflate( R.layout.item_food, parent, false )
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_food, parent, false)
         return FoodViewHolder( view )
 
     }
 
     override fun onBindViewHolder(holder: FoodViewHolder, position: Int) {
 
+        holder.bindData( position )
+
     }
 
     override fun getItemCount(): Int {
+
+        return data.size
 
     }
 
---
+++ app/src/main/java/com/app/recyclerview/ux/dataclass/FoodData.kt
@@ -0,0 +1,13 @@
+package com.app.recyclerview.ux.dataclass
+
+data class FoodData(
+
+    val txtSubject: String,
+    val txtCity: String,
+    val txtPrice: String,
+    val txtDistance: String,
+    val numOfRating: Int,
+    val rating: Float,
+    val urlImage: String
+
+)
